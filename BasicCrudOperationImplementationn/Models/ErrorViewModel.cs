9b91ad1425f72ec3e1b9930e4e5c67d60c6de438
user <user@DESKTOP-4PLIG0I>
2024-03-22 00:57:30 +0500

    Add project files.

---
+++ BasicCrudOperationImplementationn/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BasicCrudOperationImplementationn.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ BasicCrudOperationImplementationn/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BasicCrudOperationImplementationn.Models;
+
+public partial class Student
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Semester { get; set; }
+
+    public int? Age { get; set; }
+
+    public string? Contact { get; set; }
+
+    public string? Department { get; set; }
+}
